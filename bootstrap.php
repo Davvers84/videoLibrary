4d56c09036ee0ddbc73f57eaf3143b274513d239
Michael Davenport <michael.davenport@enta.net>
2018-11-01 13:12:03 +0000

    Moving index to public folder and started to bootstrap DB

--- bootstrap.php
+++ bootstrap.php
@@ -5,3 +5,20 @@
  * Date: 01/11/18
  * Time: 11:02
  */
+require __DIR__ . '/vendor/autoload.php';
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+$capsule->addConnection([
+    "driver" => getenv('DB_CONNECTION'),
+    "host" => getenv('DB_HOST'),
+    "database" => getenv('DB_DATABASE'),
+    "username" => getenv('DB_USERNAME'),
+    "password" => getenv('DB_PASSWORD')
+]);
+
+//Make this Capsule instance available globally.
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM.
+$capsule->bootEloquent();
---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michaeldavenport
+ * Date: 31/10/18
+ * Time: 15:34
+ */
+session_start();
+define('ROOTPATH', __DIR__ . '/..');
+require ROOTPATH . '/vendor/autoload.php';
+
+Use eftec\bladeone\BladeOne;
+use Vibrary\Controllers\RouteController;
+
+// Load Environment Vars
+$dotenv = new Dotenv\Dotenv(ROOTPATH);
+$dotenv->load();
+
+// Routing
+$route = new RouteController();
+
+// Display View
+$views = ROOTPATH . '/resources/views';
+$cache = ROOTPATH . '/resources/cache';
+$blade = new BladeOne($views, $cache,BladeOne::MODE_AUTO);
+
+// Cross Site Security Token
+if (empty($_SESSION['token'])) {
+    $_SESSION['token'] = bin2hex(random_bytes(32));
+}
+$route->data['token'] =  $_SESSION['token'];
+echo $blade->run($route->view,$route->data);
